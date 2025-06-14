6c9b880f9c56d2eb6d4be3085f17cf20de8650c0
cyber-josh25 <joshwa.miscawi@outlook.com>
2025-06-14 13:53:25 +0200

    feat: TintinRecept tillagd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String[][] TintinsRecept = new String[2][3];
+        TintinsRecept[0][0] = "Lollipop"; 
     }
 }
