761130dee4aa8b55b0ec1ab127dcdc5d6648ff90
cyber-josh25 <joshwa.miscawi@outlook.com>
2025-06-14 14:10:35 +0200

    feat: New features

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,14 @@ public class Main {
         TintinsRecept[0][0] = "Lollipop";
         TintinsRecept[0][1] = "Lollipop";
         TintinsRecept[0][2] = "Lollipop";
+
+        TintinsRecept[1][0] = "Lollipop";
+        TintinsRecept[1][1] = "Lollipop";
+        
+        for (int i = 0; i < TintinsRecept.length; i++) {
+            for (int j = 0; j < TintinsRecept[i].length; j++) {
+                System.out.println(TintinsRecept[i][j] + " ");
+            }
+        }
     }
 }
