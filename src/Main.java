6dd4e40ab6f32ab80898e5b5cdbece16a08a9dcc
cyber-josh25 <joshwa.miscawi@outlook.com>
2025-06-14 14:05:58 +0200

    feat: mer recept tillagda

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String[][] TintinsRecept = new String[2][3];
-        TintinsRecept[0][0] = "Lollipop"; 
+        TintinsRecept[0][0] = "Lollipop";
+        TintinsRecept[0][1] = "Lollipop";
+        TintinsRecept[0][2] = "Lollipop";
     }
 }
