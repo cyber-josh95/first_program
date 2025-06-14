4c1e46b655c95a05efd2b47d030345017e9a287a
cyber-josh25 <joshwa.miscawi@outlook.com>
2025-06-14 14:26:59 +0200

    feat: new feat

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
         TintinsRecept[1][0] = "banana";
         TintinsRecept[1][1] = "apple";
         TintinsRecept[1][2] = "apple";
+
+        for (int i = 0; i < TintinsRecept.length; i++) {
+            for (int j = 0; j < TintinsRecept[i].length; j++) {
+                System.out.println(TintinsRecept[i][j]);
+            }
+        }
         
     }
 }
